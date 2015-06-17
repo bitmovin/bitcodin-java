831b026c21a9dce4d599feeb2b7ccb67a53b6d9f
Christopher Mueller <christopher.mueller@bitmovin.net>
2015-06-17 14:59:44 -0700

    Added BitcodinApiTest.java

---
+++ src/test/java/com/bitmovin/bitcodin/api/test/BitcodinApiTest.java
@@ -0,0 +1,13 @@
+package com.bitmovin.bitcodin.api.test;
+
+import com.bitmovin.bitcodin.api.BitcodinApi;
+
+public class BitcodinApiTest {
+
+    public static void main(String[] args) {
+        
+        String key = "THIS_IS_MY_KEY";
+        BitcodinApi bitApi = new BitcodinApi(key);
+    }
+
+}
