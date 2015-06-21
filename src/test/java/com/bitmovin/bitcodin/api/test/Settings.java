defe2fafb2832cd5966d957d6dfc4b0fb7ff8b99
Christopher Mueller <christopher.mueller@bitmovin.net>
2015-06-20 21:35:08 -0700

    Use local_settings.json when ci settings are not available

--- src/test/java/com/bitmovin/bitcodin/api/test/Settings.java
+++ src/test/java/com/bitmovin/bitcodin/api/test/Settings.java
@@ -11,7 +11,6 @@ import com.google.gson.Gson;
 import com.google.gson.annotations.Expose;
 
 public class Settings {
-    
     @Expose
     public String apikey;
     @Expose
@@ -21,6 +20,7 @@ public class Settings {
 
     static private Settings settings = null;
     static private String settingsfile  = "settings.json";
+    static private String localSettingsfile = "local_settings.json";
     
     static public Settings getInstance() throws FileNotFoundException
     {
@@ -29,7 +29,12 @@ public class Settings {
             InputStream inputStream = Settings.class.getClassLoader().getResourceAsStream(Settings.settingsfile);
             
             if (inputStream == null)
-                throw new FileNotFoundException("Cloud not find config file: " + Settings.settingsfile);
+            {
+                inputStream = Settings.class.getClassLoader().getResourceAsStream(Settings.localSettingsfile);
+                
+                if (inputStream == null)
+                  throw new FileNotFoundException("Cloud not find any config file: " + Settings.settingsfile);
+            }
 
             BufferedReader  reader = new BufferedReader(new InputStreamReader(inputStream));
             Gson            gson   = new Gson();
