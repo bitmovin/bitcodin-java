ef819d9e590c0d8a8a9bead51cc98c4595827b81
Christopher Mueller <christopher.mueller@bitmovin.net>
2015-06-20 22:30:15 -0700

    Added sintel transcoding example

---
+++ examples/com/bitmovin/bitcodin/examples/TranscodeSintelToDASHAndHLS.java
@@ -0,0 +1,77 @@
+package com.bitmovin.bitcodin.examples;
+
+import com.bitmovin.bitcodin.api.BitcodinApi;
+import com.bitmovin.bitcodin.api.exception.BitcodinApiException;
+import com.bitmovin.bitcodin.api.input.HTTPInputConfig;
+import com.bitmovin.bitcodin.api.input.Input;
+import com.bitmovin.bitcodin.api.job.Job;
+import com.bitmovin.bitcodin.api.job.JobConfig;
+import com.bitmovin.bitcodin.api.job.JobStatus;
+import com.bitmovin.bitcodin.api.job.ManifestType;
+import com.bitmovin.bitcodin.api.media.EncodingProfile;
+import com.bitmovin.bitcodin.api.media.EncodingProfileConfig;
+import com.bitmovin.bitcodin.api.media.Preset;
+import com.bitmovin.bitcodin.api.media.Profile;
+import com.bitmovin.bitcodin.api.media.VideoStreamConfig;
+
+public class TranscodeSintelToDASHAndHLS {
+
+    public static void main(String[] args) {
+        
+        /* Create BitcodinApi */
+        String apiKey = "05790831d5131be17ae3674591c0a6de23243d05be43a89f514768de87b38c91";
+        BitcodinApi bitApi = new BitcodinApi(apiKey);
+        
+        /* Create URL Input */
+        HTTPInputConfig httpInputConfig = new HTTPInputConfig();
+        httpInputConfig.url = "http://ftp.nluug.nl/pub/graphics/blender/demo/movies/Sintel.2010.720p.mkv";
+
+        Input input;
+        try {
+            input = bitApi.createInput(httpInputConfig);
+        } catch (BitcodinApiException e) {
+            System.out.println("Could not create input: " + e.getMessage());
+            return;
+        }
+        
+        System.out.println("Created Input: " + input.filename);
+        
+        /* Create EncodingProfile */
+        VideoStreamConfig videoConfig = new VideoStreamConfig();
+        videoConfig.bitrate = 1 * 1024 * 1024;
+        videoConfig.width = 640;
+        videoConfig.height = 480;
+        videoConfig.profile = Profile.MAIN;
+        videoConfig.preset = Preset.STANDARD;
+
+        EncodingProfileConfig encodingProfileConfig = new EncodingProfileConfig();
+        encodingProfileConfig.name = "JUnitTestProfile";
+        encodingProfileConfig.videoStreamConfigs.add(videoConfig);
+        
+        EncodingProfile encodingProfile;
+        try {
+            encodingProfile = bitApi.createEncodingProfile(encodingProfileConfig);
+        } catch (BitcodinApiException e) {
+            System.out.println("Could not create encoding profile: " + e.getMessage());
+            return;
+        }
+        
+        /* Create Job */
+        JobConfig jobConfig = new JobConfig();
+        jobConfig.encodingProfileId = encodingProfile.encodingProfileId;
+        jobConfig.inputId = input.inputId;
+        jobConfig.manifestTypes.addElement(ManifestType.MPEG_DASH_MPD);
+        jobConfig.manifestTypes.addElement(ManifestType.HLS_M3U8);
+
+        Job job;
+        try {
+            job = bitApi.createJob(jobConfig);
+        } catch (BitcodinApiException e) {
+            System.out.println("Could not create job: " + e.getMessage());
+            return;
+        }
+        
+        System.out.println("Job with ID " + job.jobId + " submitted successfully!");
+    }
+
+}
