2d59826a9ce104910dfd31e59aaf624c0040c4c8
Christopher Mueller <christopher.mueller@bitmovin.net>
2015-06-23 17:27:52 -0700

    Statistic: added values

--- src/main/java/com/bitmovin/bitcodin/api/statistics/Statistic.java
+++ src/main/java/com/bitmovin/bitcodin/api/statistics/Statistic.java
@@ -4,5 +4,21 @@ import com.google.gson.annotations.Expose;
 
 public class Statistic {
     @Expose
+    public double averageDurationInSeconds;
+    @Expose
     public int jobsFinished;
+    @Expose
+    public int durationAllInSeconds;
+    @Expose
+    public int allJobs;
+    @Expose
+    public int jobsUnfinished;
+    @Expose
+    public double workloadPercentage;
+    @Expose
+    public int timeWindowInSeconds;
+    @Expose
+    public int enqueueDuration;
+    @Expose
+    public double averageEnqueueDuration;
 }
