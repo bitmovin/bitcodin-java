4cb1563d10e334abb7249eeb55ce595406116896
Dominic Miglar <dominic.miglar@w1r3.net>
2015-08-26 13:25:35 +0200

    added examples for Closed Captions, HLS Encryption, Widevine DRM, Playready DRM, Widevine+Playready Combined DRM, HLS Encryption+Widevine+Playready DRM, Multiple Audio Streams

---
+++ examples/com/bitmovin/bitcodin/examples/CreateJobWithPlayreadyDRM.java
@@ -0,0 +1,112 @@
+package com.bitmovin.bitcodin.examples;
+
+import com.bitmovin.bitcodin.api.BitcodinApi;
+import com.bitmovin.bitcodin.api.exception.BitcodinApiException;
+import com.bitmovin.bitcodin.api.input.HTTPInputConfig;
+import com.bitmovin.bitcodin.api.input.Input;
+import com.bitmovin.bitcodin.api.job.*;
+import com.bitmovin.bitcodin.api.media.*;
+
+/**
+ * Created by Dominic Miglar <dominic.miglar@bitmovin.net> on 8/26/15.
+ */
+public class CreateJobWithPlayreadyDRM {
+    public static void main(String[] args) throws InterruptedException {
+
+        /* Create BitcodinApi */
+        String apiKey = "YOUR_API_KEY";
+        BitcodinApi bitApi = new BitcodinApi(apiKey);
+
+        /* Create URL Input */
+        HTTPInputConfig httpInputConfig = new HTTPInputConfig();
+        httpInputConfig.url = "http://bitbucketireland.s3.amazonaws.com/h264_720p_mp_3.1_3mbps_aac_shrinkage.mp4";
+
+        Input input;
+        try {
+            input = bitApi.createInput(httpInputConfig);
+        } catch (BitcodinApiException e) {
+            System.out.println("Could not create input: " + e.getMessage());
+            return;
+        }
+
+        System.out.println("Created Input: " + input.filename);
+
+        /* Create EncodingProfile */
+        VideoStreamConfig videoConfig = new VideoStreamConfig();
+        videoConfig.bitrate = 1 * 1024 * 1024;
+        videoConfig.width = 640;
+        videoConfig.height = 480;
+        videoConfig.profile = Profile.MAIN;
+        videoConfig.preset = Preset.STANDARD;
+
+        AudioStreamConfig audioStreamConfig = new AudioStreamConfig();
+        audioStreamConfig.defaultStreamId = 0;
+        audioStreamConfig.bitrate = 192000;
+
+        EncodingProfileConfig encodingProfileConfig = new EncodingProfileConfig();
+        encodingProfileConfig.name = "JUnitTestProfile";
+        encodingProfileConfig.videoStreamConfigs.add(videoConfig);
+        encodingProfileConfig.audioStreamConfigs.add(audioStreamConfig);
+
+        EncodingProfile encodingProfile;
+        try {
+            encodingProfile = bitApi.createEncodingProfile(encodingProfileConfig);
+        } catch (BitcodinApiException e) {
+            System.out.println("Could not create encoding profile: " + e.getMessage());
+            return;
+        }
+
+        /* Create PlayReadyDrmConfig */
+
+        PlayReadyDrmConfig drmConfig = new PlayReadyDrmConfig();
+        drmConfig.system = DrmSystem.PLAYREADY;
+        drmConfig.keySeed = "XVBovsmzhP9gRIZxWfFta3VVRPzVEWmJsazEJ46I";
+        drmConfig.laUrl = "http://playready.directtaps.net/pr/svc/rightsmanager.asmx";
+        drmConfig.method = DrmMethod.MPEG_CENC;
+        drmConfig.kid = "746573745f69645f4639465043304e4f";
+
+        /* Create Job */
+        JobConfig jobConfig = new JobConfig();
+        jobConfig.encodingProfileId = encodingProfile.encodingProfileId;
+        jobConfig.inputId = input.inputId;
+        jobConfig.manifestTypes.addElement(ManifestType.MPEG_DASH_MPD);
+        jobConfig.manifestTypes.addElement(ManifestType.HLS_M3U8);
+        jobConfig.speed = Speed.STANDARD;
+        jobConfig.drmConfig = drmConfig;
+
+        Job job;
+        try {
+            job = bitApi.createJob(jobConfig);
+        } catch (BitcodinApiException e) {
+            System.out.println("Could not create job: " + e.getMessage());
+            return;
+        }
+
+        JobDetails jobDetails;
+
+        do {
+            try {
+                jobDetails = bitApi.getJobDetails(job.jobId);
+                System.out.println("Status: " + jobDetails.status.toString() +
+                        " - Enqueued Duration: " + jobDetails.enqueueDuration + "s" +
+                        " - Realtime Factor: " + jobDetails.realtimeFactor +
+                        " - Encoded Duration: " + jobDetails.encodedDuration + "s" +
+                        " - Output: " + jobDetails.bytesWritten/1024/1024 + "MB");
+            } catch (BitcodinApiException e) {
+                System.out.println("Could not get any job details");
+                return;
+            }
+
+            if (jobDetails.status == JobStatus.ERROR) {
+                System.out.println("Error during transcoding");
+                return;
+            }
+
+            Thread.sleep(2000);
+
+        } while (jobDetails.status != JobStatus.FINISHED);
+
+        System.out.println("Job with ID " + job.jobId + " finished successfully!");
+    }
+
+}
