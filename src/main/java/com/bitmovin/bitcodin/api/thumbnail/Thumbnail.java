53a98f5c3c3b90c74fe534065db50b235e434ad8
David Moser <david.moser@perau.at>
2015-11-13 11:08:33 +0100

    Added Thumbnail.java ThumbnailConfig.java Added method createThumbnail to BitcodinApi.java Added example CreateThumbnail.java

---
+++ src/main/java/com/bitmovin/bitcodin/api/thumbnail/Thumbnail.java
@@ -0,0 +1,13 @@
+package com.bitmovin.bitcodin.api.thumbnail;
+
+import com.google.gson.annotations.Expose;
+
+/**
+ * Created by
+ * dmoser [david.moser@bitmovin.net]
+ * on 13.11.15
+ */
+public class Thumbnail {
+    @Expose
+    public String thumbnailUrl;
+}
