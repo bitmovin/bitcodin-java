c4aa20580a3d7b2e1876780ae9c732c4ebcf7957
David Moser <david.moser@perau.at>
2015-11-30 16:29:14 +0100

    Added example to create vtt hls and vtt mpd with output filename.

--- src/main/java/com/bitmovin/bitcodin/api/manifest/VttHlsConfig.java
+++ src/main/java/com/bitmovin/bitcodin/api/manifest/VttHlsConfig.java
@@ -2,7 +2,12 @@ package com.bitmovin.bitcodin.api.manifest;
 
 import com.google.gson.annotations.Expose;
 
-public class VttMpdConfig
+/**
+ * Created by
+ * dmoser [david.moser@bitmovin.net]
+ * on 30.11.15
+ */
+public class VttHlsConfig
 {
     @Expose
     public int jobId;
